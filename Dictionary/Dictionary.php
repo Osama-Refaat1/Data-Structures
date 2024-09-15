878a15a660cac34446441f59e790b062cbe059c6
Osama <osamarefaat93@gmail.com>
2024-09-15 04:26:25 +0300

    implemented Dictionary

--- Dictionary/Dictionary.php
+++ Dictionary/Dictionary.php
@@ -1,18 +1,53 @@
 <?php
-include './KeyValuePair.php';
-
- class Dictionary 
+require_once 'KeyValuePair.php';
+class Dictionary
 {
- 
+    private $entries;
 
- private KeyValuePair $entries;
-private int $entriesCount;
-public function __construct()
-{
-$this->entries =new KeyValuePair();
-$this->entriesCount = 0;
+    public function __construct()
+    {
+        $this->entries = [];
+    }
+
+    public function add(string $key, $value): void
+    {
+        if (array_key_exists($key, $this->entries)) {
+            $this->entries[$key]->setValue($value);
+        } else {
+            $this->entries[$key] = new KeyValuePair($key, $value);
+        }
+    }
+
+    public function get(string $key)
+    {
+        if (array_key_exists($key, $this->entries)) {
+            return $this->entries[$key]->getValue(); 
+        }
+        return  "Key not found" . PHP_EOL;
+    }
+
+    public function remove(string $key): void
+    {
+        if (array_key_exists($key, $this->entries)) {
+            unset($this->entries[$key]);
+            echo "KeyValuePair removed" . PHP_EOL;
+        }
+    }
+
+    public function containsKey($key): bool
+    {
+        
+        return array_key_exists($key, $this->entries);
+    }
+
+    public function size(): int
+    {
+        return count($this->entries);
+    }
+    public function print(): void
+    {
+        foreach ($this->entries as $key => $value) {
+            echo $key . " => " . $value->getValue() . PHP_EOL;
+        }
+    }
 }
-
-
-
-}//end of Dictionary class
